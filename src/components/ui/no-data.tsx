c99666150ed433bf8d19f3f02a88f3a89336b023
taivo-digitalfortress <tai.vo@digitalfortress.team>
2024-11-14 10:00:17 +0700

    feat: Reconfigure global color variables for SpaceDF

--- src/components/ui/no-data.tsx
+++ src/components/ui/no-data.tsx
@@ -23,7 +23,7 @@ export const Nodata = ({
       >
         <ImageWithBlur src={NodataSVG} alt="nodata" className="h-full w-full" />
       </div>
-      <p className="text-brand-component-text-dark mt-3 text-wrap text-center text-base font-normal">
+      <p className="mt-3 text-wrap text-center text-base font-normal text-brand-component-text-dark">
         {content || 'No Data yet'}
       </p>
     </div>
