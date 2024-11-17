66c92efde7137a57a5af85a91b902f36fecf643e
taivo-digitalfortress <tai.vo@digitalfortress.team>
2024-11-17 11:20:21 +0700

    fix: bug fix tickets 11185, 11159 3601

--- src/components/ui/alert-dialog.tsx
+++ src/components/ui/alert-dialog.tsx
@@ -32,7 +32,7 @@ const AlertDialogContent = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Content>
 >(({ className, ...props }, ref) => (
   <AlertDialogPortal>
-    <AlertDialogOverlay />
+    <AlertDialogOverlay className="bg-black/40 backdrop-blur-md" />
     <AlertDialogPrimitive.Content
       ref={ref}
       className={cn(
--- src/containers/space/create-space/create-space.tsx
+++ src/containers/space/create-space/create-space.tsx
@@ -21,8 +21,6 @@ const Settings = ({ isCreating }: { isCreating: boolean }) => {
   const t = useTranslations('space')
   const form = useFormContext<SpaceFormValues>()
 
-  const { isDirty, isValid } = form.formState
-
   return (
     <>
       <div className="flex size-full items-center justify-center">
@@ -77,8 +75,7 @@ const Settings = ({ isCreating }: { isCreating: boolean }) => {
           <div>
             <Button
               type="submit"
-              className="h-12 items-center rounded-lg border-4 border-brand-heading bg-brand-fill-outermost font-medium text-white shadow-sm dark:border-brand-stroke-outermost"
-              disabled={!isDirty || !isValid}
+              className="h-12 items-center rounded-lg border-2 border-brand-heading bg-brand-fill-outermost text-base font-semibold text-white shadow-sm dark:border-brand-stroke-outermost"
               loading={isCreating}
             >
               {t('create_space')}
