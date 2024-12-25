6c78f3998fab444a4519cf983d9db7d3efc9bdc1
huy.phan <huy.phan@digitalfortress.dev>
2024-12-25 17:22:30 +0700

    feat: Define Widget Atributes and Behaviors Using OOP

---
+++ src/widget-models/chart.ts
@@ -0,0 +1,81 @@
+enum ChartType {
+  LineChart = 'line-chart',
+  AreaChart = 'area-chart',
+  BarChart = 'bar-chart',
+}
+
+class Axes {
+  y_axis: object[]
+  is_show_grid: boolean
+  format: string
+
+  constructor(y_axis: object[], is_show_grid: boolean, format: string) {
+    this.y_axis = y_axis
+    this.is_show_grid = is_show_grid
+    this.format = format
+  }
+}
+
+class ChartSources extends Sources {
+  field: string
+  legend: string
+  color: string
+  chart_type: ChartType
+  constructor(
+    device_id: string,
+    device_type: string,
+    field: string,
+    legend: string,
+    color: string,
+    chart_type: ChartType,
+  ) {
+    super(device_id, device_type)
+    this.field = field
+    this.legend = legend
+    this.color = color
+    this.chart_type = chart_type
+  }
+}
+
+class ChartTimeFrame extends TimeFrame {
+  date_range: string
+  is_show_time_frame: boolean
+  legend: string
+  color: string
+  chart_type: ChartType
+
+  constructor(
+    date_range: string,
+    is_show_time_frame: boolean,
+    legend: string,
+    color: string,
+    chart_type: ChartType,
+    aggregation_function: string,
+  ) {
+    super(aggregation_function)
+    this.date_range = date_range
+    this.is_show_time_frame = is_show_time_frame
+    this.legend = legend
+    this.color = color
+    this.chart_type = chart_type
+  }
+}
+
+class Chart {
+  sources: ChartSources
+  axes: Axes
+  time_frame: ChartTimeFrame
+  widget_info: WidgetInfo
+
+  constructor(
+    sources: ChartSources,
+    axes: Axes,
+    time_frame: ChartTimeFrame,
+    widget_info: WidgetInfo,
+  ) {
+    this.sources = sources
+    this.axes = axes
+    this.widget_info = widget_info
+    this.time_frame = time_frame
+  }
+}
---
+++ src/widget-models/map.ts
@@ -0,0 +1,8 @@
+class WidgetMap {
+  sources: Sources
+  widget_info: WidgetInfo
+  constructor(sources: Sources, widget_info: WidgetInfo) {
+    this.sources = sources
+    this.widget_info = widget_info
+  }
+}
