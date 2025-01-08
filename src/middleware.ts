86374e9b59171ea7e992c6b6511ae09a8ced6cdc
kinhdev24 <kinhdev24@gmail.com>
2025-01-08 18:31:51 +0700

    handle logics subdomain

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,46 +1,64 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 import createMiddleware from 'next-intl/middleware'
+import { cookies } from 'next/headers'
 import { locales } from './i18n/request'
 import { Locale } from './types/global'
 import { getSubdomain } from './utils'
-import { notFound } from 'next/navigation'
-import { cookies } from 'next/headers'
+import { getValidSubdomain } from './utils/subdomain'
+
+// RegExp for public files
+const PUBLIC_FILE = /\.(.*)$/ // Files
 
 export default async function middleware(request: NextRequest) {
+  const url = request.nextUrl.clone()
+
+  if (PUBLIC_FILE.test(url.pathname) || url.pathname.includes('_next')) return
+
   // Step 1: Use the incoming request (example)
   const defaultLocale = (cookies().get('NEXT_LOCALE')?.value || 'en') as Locale
 
-  const [, locale, ...segments] = request.nextUrl.pathname.split('/')
+  let [, locale, ...segments] = request.nextUrl.pathname.split('/')
 
-  const headerHost = request.headers.get('x-forwarded-host')
-  const rootHost = request.nextUrl.host
+  const host = request.headers.get('host')
 
   // Step 2: Create and call the next-intl middleware (example)
   const handleI18nRouting = createMiddleware({
     locales,
     defaultLocale,
+    // localePrefix: 'as-needed',
   })
 
-  if (
-    locale != null &&
-    !segments.length &&
-    locales.includes(locale as Locale)
-  ) {
-    request.nextUrl.pathname = `/${locale}/digital-twins`
+  const subdomain = getValidSubdomain(host)
+
+  // segments = segments.filter((seg) => seg !== subdomain)
+
+  if (subdomain) {
+    let pathWithoutLocale = url.pathname.replace(`/${locale}`, '')
+
+    // Subdomain available, rewriting
+
+    if (
+      locale != null &&
+      !segments.length &&
+      locales.includes(locale as Locale)
+    ) {
+      pathWithoutLocale = `/digital-twins`
+    }
+
+    let rewriteDomain = `/${locale}/${subdomain}${pathWithoutLocale}`
+
+    console.log({ rewriteDomain })
+
+    request.nextUrl.pathname = rewriteDomain
+
+    return handleI18nRouting(request)
   }
 
   // Handle the locale routing
   const response = handleI18nRouting(request)
-  let env = 'production'
-  const organization = getSubdomain(headerHost || '', rootHost || '')
 
-  if (headerHost?.startsWith('dev')) {
-    env = 'development'
-  }
-
-  response.cookies.set('organization', organization)
-  response.cookies.set('env', env)
+  response.cookies.set('organization', subdomain || '')
 
   return response
 }
