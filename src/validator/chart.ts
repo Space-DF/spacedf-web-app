8165ec9e86f0ac4b5921bd3d055664dca272b478
huy.phan <huy.phan@digitalfortress.dev>
2025-01-09 15:31:46 +0700

    feat: 12145 Charts - Axes Atributes

--- src/validator/chart.ts
+++ src/validator/chart.ts
@@ -1,4 +1,4 @@
-import { ChartType } from '@/widget-models/chart'
+import { ChartType, Orientation } from '@/widget-models/chart'
 import { z } from 'zod'
 
 export const sourceChartSchema = z.object({
@@ -25,6 +25,15 @@ export const sourceChartSchema = z.object({
       show_value: z.boolean(),
     }),
   }),
+  axes: z.object({
+    y_axis: z.object({
+      orientation: z.enum([Orientation.Left, Orientation.Right]),
+      unit: z.string().max(50, 'Maximum 50 characters').optional(),
+    }),
+    hide_axis: z.boolean(),
+    is_show_grid: z.boolean(),
+    format: z.string(),
+  }),
 })
 
 export type SourceChartPayload = z.infer<typeof sourceChartSchema>
@@ -34,7 +43,7 @@ export const defaultSourceChartValues: SourceChartPayload['sources'] = [
     device_id: '1',
     field: '1',
     legend: 'Temperature',
-    color: '171A28',
+    color: 'default',
     chart_type: ChartType.LineChart,
     show_legend: true,
   },
