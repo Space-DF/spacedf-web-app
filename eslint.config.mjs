9bd9d10201bbf8d2f390d9d55dd338599a9e58c1
kinhdev24 <kinhdev24@gmail.com>
2025-01-11 15:01:05 +0700

    chore: refactored eslit

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import { FlatCompat } from '@eslint/eslintrc'
+const compat = new FlatCompat({
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+})
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next', 'next/core-web-vitals', 'next/typescript'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      'react-hooks/exhaustive-deps': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_', // Ignore arguments starting with "_"
+          varsIgnorePattern: '^_', // Ignore variables starting with "_"
+          ignoreRestSiblings: true, // Ignore destructured siblings
+        },
+      ],
+    },
+  }),
+]
+export default eslintConfig
