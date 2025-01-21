67bb99f6395785a2d37ce0d21ece74a6bb0ae088
taivo-digitalfortress <tai.vo@digitalfortress.team>
2025-01-21 09:44:06 +0700

    feat: landing page preview

---
+++ src/app/[locale]/(landing)/(withLayout)/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import Lottie from 'lottie-react'
+import * as lottieFile from './video.json'
+
+export default function LandingPage() {
+  return (
+    <>
+      <Lottie animationData={lottieFile} />
+    </>
+  )
+}
