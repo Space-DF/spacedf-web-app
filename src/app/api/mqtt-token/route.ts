8c851d6b377f5159024698c1f0cb2a4857cc4a19
huy.phan <huy.phan@digitalfortress.dev>
2025-11-11 08:54:47 +0700

    feat: mqtt authorization

--- src/app/api/mqtt-token/route.ts
+++ src/app/api/mqtt-token/route.ts
@@ -1,21 +1,7 @@
-import { decode } from 'next-auth/jwt'
-import { cookies } from 'next/headers'
+import { readSession } from '@/utils/server-actions'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-  const sessionCookie = cookies().get('next-auth.session-token')?.value
-  if (!sessionCookie) {
-    return NextResponse.json(
-      { error: 'No session cookie found' },
-      { status: 401 }
-    )
-  }
-
-  const decoded = await decode({
-    token: sessionCookie,
-    secret: process.env.NEXTAUTH_SECRET || '',
-    salt: '',
-  })
-
-  return NextResponse.json({ mqtt_token: decoded?.access })
+  const sessionCookie = await readSession()
+  return NextResponse.json({ mqtt_token: sessionCookie?.user?.access })
 }
