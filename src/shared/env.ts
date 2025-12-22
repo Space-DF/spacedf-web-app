4acad8fa1da0348d5e2e405cf01effcdd28503e7
quynhnguyen <quynh.nguyen@digitalfortress.dev>
2025-12-22 14:34:45 +0700

    chore: Update logic disable map on development

--- src/shared/env.ts
+++ src/shared/env.ts
@@ -18,3 +18,8 @@ export const ADMIN_SITE_URL =
   process.env.NEXT_PUBLIC_ADMIN_SITE_URL || 'https://spacedf.com'
 
 export const SPACEDF_DEV_PASSWORD = process.env.SPACEDF_DEV_PASSWORD || ''
+
+export const NEXT_PUBLIC_NODE_ENV =
+  process.env.NEXT_PUBLIC_NODE_ENV || 'development'
+
+export const NEXT_PUBLIC_MAP_ENABLED = process.env.NEXT_PUBLIC_MAP_ENABLED
