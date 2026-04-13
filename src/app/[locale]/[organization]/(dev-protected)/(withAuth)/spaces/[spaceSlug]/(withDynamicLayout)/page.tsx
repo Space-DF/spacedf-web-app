1c9a1b0cf3284e059958cfa54602c400d5cc840c
huy.phan <huy.phan@digitalfortress.dev>
2026-04-13 09:52:55 +0700

    chore: update missing file

--- src/app/[locale]/[organization]/(dev-protected)/(withAuth)/spaces/[spaceSlug]/(withDynamicLayout)/page.tsx
+++ src/app/[locale]/[organization]/(dev-protected)/(withAuth)/spaces/[spaceSlug]/(withDynamicLayout)/page.tsx
@@ -2,17 +2,24 @@ import { checkSlugName } from '@/lib/organizations'
 import { getValidSubdomain } from '@/utils/subdomain'
 import dynamic from 'next/dynamic'
 import { headers } from 'next/headers'
-const FleetTracking = dynamic(() => import('@/templates/fleet-tracking'), {
-  ssr: false,
-})
-const SmartBuilding = dynamic(() => import('@/templates/smart-building'), {
-  ssr: false,
-})
+
+const templateImporters = {
+  'fleet-tracking': () => import('@/templates/fleet-tracking'),
+  'smart-building': () => import('@/templates/smart-building'),
+} as const
+
 export default async function DigitalTwins() {
   const headersList = headers()
   const host = headersList.get('host') || 'localhost'
 
   const org = await getValidSubdomain(host)
   const { template } = await checkSlugName(org || '')
-  return template === 'smart_building' ? <SmartBuilding /> : <FleetTracking />
+
+  const importer =
+    templateImporters[template as keyof typeof templateImporters] ||
+    templateImporters['fleet-tracking']
+  const Template = dynamic(importer, {
+    ssr: false,
+  })
+  return <Template />
 }
