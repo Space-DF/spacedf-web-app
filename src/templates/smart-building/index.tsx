7e9da05b4217f59f58fdfa77e358498705d23083
Quynh-Nguyen <quynh.nguyen@digitalfortress.dev>
2026-05-12 10:03:20 +0000

    Trigger Build

--- src/templates/smart-building/index.tsx
+++ src/templates/smart-building/index.tsx
@@ -8,7 +8,7 @@ import {
 } from '@/components/ui/dropdown-menu'
 import { useAuthenticated } from '@/hooks/useAuthenticated'
 import { useAddDeviceStore } from '@/stores/template/add-device'
-import { getS3Url } from '@/utils'
+import { getS3Url, setCookie } from '@/utils'
 import { Canvas } from '@react-three/fiber'
 import Image from 'next/image'
 import { Suspense, useEffect, useMemo, useRef, useState } from 'react'
@@ -16,10 +16,11 @@ import { DialogSelectDeviceFromList } from './components/dialog-select-device-fr
 import ThreeModel, { ModelFallback } from './components/three-glb-model'
 import { ThreeModelControls } from './components/three-model-controls'
 import { DropdownSwitchBuilding } from './components/three-model-controls/components/dropdown-switch-building'
-import { useGlobalStore } from '@/stores'
+import { getNewLayouts, useGlobalStore, useLayout } from '@/stores'
 import { List } from 'lucide-react'
 import { useTranslations } from 'next-intl'
 import { useShallow } from 'zustand/react/shallow'
+import { COOKIES, NavigationEnums } from '@/constants'
 
 export default function SmartBuilding() {
   const t = useTranslations('smartBuilding')
@@ -41,6 +42,15 @@ export default function SmartBuilding() {
     modelPoint: { x: number; y: number; z: number }
   } | null>(null)
   const [selectDeviceDialogOpen, setSelectDeviceDialogOpen] = useState(false)
+  const { toggleDynamicLayout, dynamicLayouts } = useLayout(
+    useShallow((state) => ({
+      toggleDynamicLayout: state.toggleDynamicLayout,
+      dynamicLayouts: state.dynamicLayouts,
+      setCookieDirty: state.setCookieDirty,
+    }))
+  )
+
+  const isDeviceTabVisible = dynamicLayouts.includes(NavigationEnums.DEVICES)
 
   const isAuthenticated = useAuthenticated()
 
@@ -64,6 +74,11 @@ export default function SmartBuilding() {
   const handleAddDevice = (event: Event) => {
     if (!contextMenuPosition) return
     event.preventDefault()
+    if (!isDeviceTabVisible) {
+      const newLayout = getNewLayouts(dynamicLayouts, NavigationEnums.DEVICES)
+      setCookie(COOKIES.DYNAMIC_LAYOUTS, newLayout)
+      toggleDynamicLayout(NavigationEnums.DEVICES)
+    }
     setIsOpenDeviceModal(true)
     setDeviceModalPosition({
       x: contextMenuPosition.modelPoint.x,
