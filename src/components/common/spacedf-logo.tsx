8d3185b7ba896cb8c8e601ea8b4c4917bf35602e
huy.phan <huy.phan@digitalfortress.dev>
2026-06-11 14:40:07 +0700

    chore: update new get icon logic

--- src/components/common/spacedf-logo.tsx
+++ src/components/common/spacedf-logo.tsx
@@ -11,7 +11,6 @@ const SpacedfLogo = () => {
   const { mounted } = useMounted()
 
   const currentTheme = mounted ? resolvedTheme : 'light'
-  const isLight = currentTheme === 'light'
 
   const logoUrl = (() => {
     if (!setting?.themes || setting.themes.length === 0) {
@@ -27,39 +26,23 @@ const SpacedfLogo = () => {
 
   return (
     <div className="pointer-events-auto">
-      <div className="bg-gradient-to-r from-[#6E4AFF] to-[#CCBFFF] p-[1px] rounded-lg">
-        <div
-          className="w-32 h-[38px] p-2 rounded-lg bg-gradient-to-b from-[#171a28b3] to-[#1f2336b3] bg-white dark:bg-brand-fill-outermost backdrop-blur-xs pointer-events-none"
-          style={
-            isLight
-              ? {
-                  background:
-                    'linear-gradient(180deg, #C8C8C8 0%, #7B7B7B 100%)',
-                }
-              : undefined
-          }
-        >
-          <div className="flex items-center justify-center h-full">
-            {logoUrl ? (
-              <Image
-                width={114}
-                height={24}
-                src={logoUrl}
-                alt={setting?.brand_name || 'spacedf-logo'}
-                className="max-w-full max-h-full object-contain"
-              />
-            ) : (
-              <Image
-                src="/images/spacedf-logo.svg"
-                alt="spacedf-logo"
-                width={114}
-                height={24}
-                priority
-              />
-            )}
-          </div>
-        </div>
-      </div>
+      {logoUrl ? (
+        <Image
+          width={114}
+          height={24}
+          src={logoUrl}
+          alt={setting?.brand_name || 'spacedf-logo'}
+          className="max-w-full max-h-full object-contain"
+        />
+      ) : (
+        <Image
+          src="/images/spacedf-logo.svg"
+          alt="spacedf-logo"
+          width={114}
+          height={24}
+          priority
+        />
+      )}
     </div>
   )
 }
