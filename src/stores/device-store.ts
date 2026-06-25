9b72a826ea0c4808053c16f25f01414cc05bff98
kinhdev24 <kinhdev24@gmail.com>
2026-06-25 12:06:00 +0700

    fixed: Water deepth animation when level changes

--- src/stores/device-store.ts
+++ src/stores/device-store.ts
@@ -152,9 +152,16 @@ export const useDeviceStore = create<DeviceModelState & DeviceModelAction>()(
     },
 
     setDevicesFleetTracking: (data) => {
-      return set(() => ({
-        devicesFleetTracking: reduceDeviceFleetTracking(data),
-      }))
+      set((state) => {
+        const incoming = reduceDeviceFleetTracking(data)
+
+        return {
+          devicesFleetTracking: {
+            ...incoming,
+            ...state.devicesFleetTracking,
+          },
+        }
+      })
     },
 
     setDeviceProperties: (deviceId, data) => {
